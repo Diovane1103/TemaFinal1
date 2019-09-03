f088ab8c021f876c94f2550cfd65c0164d5b6e59
Diovane <diovane.rossato@ilegra.com>
2019-09-03 13:25:39 -0300

    Fazendo testes

--- src/test/java/com/cloud/desafioFinal/MensagemServiceTest.java
+++ src/test/java/com/cloud/desafioFinal/MensagemServiceTest.java
@@ -1,14 +1,22 @@
 package com.cloud.desafioFinal;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class MensagemServiceTest {
 
+    @Autowired
+    private MensagemService mensagemService;
+
     @Test
-    public void getMensagem() {
+    public void getMensagemTest() {
+        assertEquals("Hello World!", mensagemService.getMensagem());
     }
 }
